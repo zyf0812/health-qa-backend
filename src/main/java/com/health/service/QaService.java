7b72cc921c6fd5dc2146b2330191c57e3d720f30
zyf <2175601317@qq.com>
2025-10-17 20:20:53 +0800

    type为空时查询所有实体

--- src/main/java/com/health/service/QaService.java
+++ src/main/java/com/health/service/QaService.java
@@ -12,12 +12,10 @@ public class QaService {
 
     // 问答查询业务方法
     public String getAnswer(String question) {
-        //测试异常
-        throw new RuntimeException("测试异常");
         // 调用Repository查询
-//        String answer = qaRepository.findAnswerByQuestion(question);
-//        // 处理无结果场景
-//        return answer == null ? "暂无相关健康知识，可尝试提问：“感冒症状”“高血压饮食”" : answer;
+        String answer = qaRepository.findAnswerByQuestion(question);
+        // 处理无结果场景
+        return answer == null ? "暂无相关健康知识，可尝试提问：“感冒症状”“高血压饮食”" : answer;
     }
 }
 
